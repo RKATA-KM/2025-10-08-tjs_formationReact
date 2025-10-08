c67be14059b3ddebb9b7baccac68fb8970cf2e20
RomainKATA <romain.kata@konicaminolta.fr>
2025-10-08 14:38:45 +0100

    Ajout des boutons et css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Button from '../ui/Button/Button'
+
+const App=(props)=>{
+  console.trace(props);
+  return (
+  <>
+    <div>DEMAT Breizh</div>
+    <Button value="Benjamin"/>
+
+    <Button value="Benjamin"/>
+    <br />
+    <Button value="Benjamin"/>
+
+    <Button value="Benjamin"/>
+</>
+  )
+}
+
+export default App
